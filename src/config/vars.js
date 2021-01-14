60622b7bcad76b7db1f7dd4997f5db70af312bd5
Hayyan Hami <hayyanhami@outlook.com>
2021-01-14 08:54:51 +0330

    add radix, count sorts to cslang

--- src/config/vars.js
+++ src/config/vars.js
@@ -1,19 +1,7 @@
-import { algorithms, authors, formats, languages, threads } from "./types"
+import { algorithms, languages } from "./types";
 
 const app = window.electron.remote.app;
 export const wdir = `${app.getAppPath()}\\programs\\`;
-console.log(wdir)
-
-export const programs = [
-  {
-    algo: algorithms.merge, // can be anything depending on your app
-    author: authors.hayyaun,
-    format: formats.py,
-    lang: languages.python,
-    thread: threads.single,
-    name: "Merge Sort (Python)"
-  }
-]
 
 export const authorHasLangs = {
   hayyaun: [languages.python, languages.cs],
